e080a630eda667c432d2996906490abae497e102
Yellow <cococing0324@gmail.com>
2019-10-21 02:36:26 +0800

    HW3初稿

---
+++ HW3/Q2.m
@@ -0,0 +1,22 @@
+clear;clc;
+num=8;
+for j=1:num
+    theta=j*(2*pi/num);
+    x1array(1)=real(1*exp(1i*theta)); 
+    x2array(1)=imag(1*exp(1i*theta));
+
+    for i=1:10000
+        x(1)=x1array(i); x(2)=x2array(i);
+        u=((4*x(1)*x(1)+x(2)*x(2)-4)-1);
+        x1dot=x(2)-(4*x(1)*x(1)+x(2)*x(2)-4)*x(1)+u*x(1);
+        x2dot=-x(1)-(4*x(1)*x(1)+x(2)*x(2)-4)*x(2)+u*x(2);
+        x1array(i+1)=x(1)+x1dot*0.001;
+        x2array(i+1)=x(2)+x2dot*0.001;
+    end
+
+    xlabel('x(1)');
+    ylabel('x(2)');
+    plot(x1array,x2array);
+    title('Q2');
+    hold on;
+end
